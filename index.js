403a32e04d94c7d5c6d98b2d651a77558abbdfa4
ykrueng <yasirah.krueng@gmail.com>
2019-01-08 15:24:46 -0600

    refactor POST /api/posts to use async await and custom middleware

--- index.js
+++ index.js
@@ -1,33 +1,49 @@
 // import your node modules
 const express = require("express");
-const bodyParser = require("body-parser");
-const cors = require("cors");
 const db = require("./data/db.js");
 
-// add your server code starting here
+// middlewares
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const validatePostReq = (req, res, next) => {
+  const { title, contents } = req.body;
+
+  // check whether title or contents is empty
+  if (!title || !contents) {
+    res
+      .status(400)
+      .json({
+        errorMessage: "Please provide title and contents for the post."
+      });
+  }
+
+  next();
+}
+
+// applying middlewares
 const server = express();
-server.use(bodyParser.json());
 server.use(cors());
+server.use(bodyParser.json());
 
 server.get("/", (req, res) => {
   res.status(200).json({ message: "Welcome to my first API" });
 });
 
-server.post("/api/posts", (req, res) => {
+server.post("/api/posts", validatePostReq, async (req, res) => {
   const { title, contents } = req.body;
 
-  if (!title || !contents) {
-    res
-      .status(400)
-      .json({ error: "Did not receive the required title or contents" });
-  } else {
-    db.insert({ title, contents })
-      .then(postId => res.status(200).json(postId))
-      .catch(error => res.status(500).json(error));
+  try {
+    const { id } = await db.insert({ title, contents });
+    const post = await db.findById(id);
+    res.status(201).json(post);
+  } catch (error) {
+    res.status(500).json({
+      error: "There was an error while saving the post to the database"
+    });
   }
 });
 
-server.get("/api/posts",(req, res) => {
+server.get("/api/posts", (req, res) => {
   db.find()
     .then(posts => res.status(200).json(posts))
     .catch(error => res.status(500).json(error));
@@ -57,13 +73,17 @@ server.delete("/api/posts/:id", (req, res) => {
     .then(post => {
       deletedPost = post[0];
       if (!deletedPost) {
-        res.status(404).json({ message: "The post with the specified ID does not exist." });
+        res
+          .status(404)
+          .json({ message: "The post with the specified ID does not exist." });
       } else {
-        return db.remove(id)
+        return db.remove(id);
       }
     })
     .then(docId => res.status(200).json(deletedPost))
-    .catch(err => res.status(500).json({ error: "The post could not be removed" }));
+    .catch(err =>
+      res.status(500).json({ error: "The post could not be removed" })
+    );
 });
 
 server.put("/api/posts/:id", (req, res) => {
@@ -71,30 +91,32 @@ server.put("/api/posts/:id", (req, res) => {
   const postId = req.params.id;
 
   if (!title && !contents) {
-    res.status(400).json({ message: 'Did not receive title or contents to update'})
+    res
+      .status(400)
+      .json({ message: "Did not receive title or contents to update" });
   } else {
-
     db.findById(postId)
       .then(post => {
         if (post.length === 0) {
-          res.status(404).json({ message: 'Cannot find a post with requested id' });
+          res
+            .status(404)
+            .json({ message: "Cannot find a post with requested id" });
         } else {
-          db.update(postId, {title, contents})
-            .then(num => {
-              if (num === 1) {
-                res.status(200).json({
-                  ...post[0],
-                  title,
-                  contents
-                });
-              } else {
-                res.status(500).json({ message: 'Cannot perform update' })
-              }
-            })
+          db.update(postId, { title, contents }).then(num => {
+            if (num === 1) {
+              res.status(200).json({
+                ...post[0],
+                title,
+                contents
+              });
+            } else {
+              res.status(500).json({ message: "Cannot perform update" });
+            }
+          });
         }
       })
-      .catch(error => res.status(500).json(error))
+      .catch(error => res.status(500).json(error));
   }
-})
+});
 
 server.listen(5000, () => console.log("Server is running on port: 5000"));
